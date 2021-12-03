44eb6cff40ebe641010703f3e9c11eb2b6368622
reshmasubban <60497202+reshmasubban@users.noreply.github.com>
2021-12-03 15:21:59 +0530

    Add files via upload

---
+++ fixing bug.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import sys
+betty_botter = 'Betty Botter bought a bit of butter But the bit of butter Betty Botter bought was bitter So Betty ' \
+ 'Botter bought a better bit of butter '
+expected_output = 'botter betty bought a bit of butter But the bit of butter botter betty bought was bitter So botter ' \
+ 'betty bought a better bit of butter '
+
+def botter_betty():
+    global betty_botter
+    betty_botter = 'Betty Botter bought a bit of butter But the bit of butter Betty Botter bought was bitter So Betty ' \
+    'Botter bought a better bit of butter '
+    betty_botter = betty_botter.replace("Botter", "betty")
+    betty_botter = betty_botter.replace("Betty", "botter")
+
+def main(argv):
+	botter_betty()
+	try:
+		assert betty_botter == expected_output
+		print("Success")
+	except AssertionError:
+		print("Failed")
+		exit(1)
+if __name__ == '__main__':
+    main(sys.argv)
